4823ff82dcd5c6867e3529bf29244b039a44f3f5
Arya Mirshafii <aryamirshafii@lawn-143-215-113-134.lawn.gatech.edu>
2018-02-03 23:00:41 -0500

    Initial Commit

---
+++ headingCar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  headingCar
+//
+//  Created by Arya Mirshafii on 2/3/18.
+//  Copyright © 2018 georgiaTechEngineeringForInnovationGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
